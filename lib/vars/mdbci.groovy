df3f45909ca459152e804d56acd5e377e2a65aed
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2018-03-28 12:34:07 +0300

    Initial commit

---
+++ lib/vars/mdbci.groovy
@@ -0,0 +1,30 @@
+@NonCPS def entrySet(m) {m.collect {k, v -> [key: k, value: v]}}
+
+@NonCPS
+def createJSON(filename, array) {
+    def json = JsonOutput.toJson(array)
+    json = JsonOutput.prettyPrint(json)
+    writeFile file: filename, text: json
+}
+
+def mdbciGenerate(template, name) {
+    sh "./mdbci --template ${template} generate ${name}"
+}
+
+def mdbciUp(name) {
+    return sh(returnStatus: true, script: "./mdbci up ${name}")
+}
+
+def mdbciDestroy(name) {
+    sh "./mdbci destroy ${name}"
+}
+
+def logError(message) {
+    echo "ERROR: ${message}"    
+}
+
+def logInfo(message) {
+    echo "INFO: ${message}"    
+}
+
+return this
